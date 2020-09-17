2286ac03bed02411f1704f0965292ccfb5d669f7
Luvumbu <luvumbu.n@gmail.com>
2020-09-17 11:52:15 +0200

    ajout session club

--- club.php
+++ club.php
@@ -1,19 +1,14 @@
 <?php 
 session_start();
 header("Access-Control-Allow-Origin: *");
-
  
 $servername = "localhost";
 $username = "root";
 $password = $username;
 $dbname = "all_ffa";
-
-
-
 $club_nom= $_POST["club_nom"];
 $club_region= $_POST["club_region"];
 $club_departement= $_POST["club_departement"];
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -32,8 +27,6 @@ if ($result->num_rows > 0) {
   }
 } else {
 
-
-
 // Create connection
 $conn_insert = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -43,10 +36,7 @@ if ($conn_insert->connect_error) {
 
 $sql_insert = "INSERT INTO club (club_nom, club_region,club_departement)
 VALUES ('$club_nom', '$club_region', '$club_departement')";
-
-if ($conn_insert->query($sql_insert) === TRUE) {
-
- 
+if ($conn_insert->query($sql_insert) === TRUE) { 
  
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
@@ -58,13 +48,11 @@ if ($conn_insert->query($sql_insert) === TRUE) {
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-	   	$club_id= $row["club_id"];
-		 
+	   	$club_id= $row["club_id"];		 
 		}
 	} else {
 		echo "0 results";
 	}
-
 
 echo "test error";
 // fin select 
@@ -72,15 +60,10 @@ echo "test error";
 } else {
   echo "Error: " . $sql_insert . "<br>" . $conn_insert->error;
 }
-
 $conn_insert->close();
-
 }
 $conn->close();
+// ici je vais recuperer le id du club qui est la variable $club_id
+//$club_id 
+$_SESSION["club_id"] = $club_id;
 ?>
-
-
-
-
- 
-
