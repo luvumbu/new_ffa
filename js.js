4f99f8df60cac783182e8664322170825165b58a
Luvumbu <luvumbu.n@gmail.com>
2020-09-17 08:09:21 +0200

    changer js ok

--- js.js
+++ js.js
@@ -3,23 +3,22 @@ class Information {
 		this.link = link;
 		this.identite = new FormData();
 		this.req = new XMLHttpRequest();
-		this.identite_tab = [
-		];
+		this.identite_tab = [];
 	}
 	info() {
-		return this.identite_tab; 
+		return this.identite_tab;
 	}
-	add(info, text){
-		this.identite_tab.push([info, text]); 
+	add(info, text) {
+		this.identite_tab.push([info, text]);
 	}
-	push(){
-		for(var i = 0 ; i < this.identite_tab.length ; i++){
+	push() {
+		for (var i = 0; i < this.identite_tab.length; i++) {
 			console.log(this.identite_tab[i][1]);
-			this.identite.append(this.identite_tab[i][0], this.identite_tab[i][1]);		 
-		}		
-		this.req.open("POST",this.link);
+			this.identite.append(this.identite_tab[i][0], this.identite_tab[i][1]);
+		}
+		this.req.open("POST", this.link);
 		this.req.send(this.identite);
-		console.log(this.req);	 
+		console.log(this.req);
 	}
 }
 var ok = new Information("php.php"); // création de la classe 
@@ -53,155 +52,168 @@ var tr = document.getElementsByTagName("tr");
 
 
 
-var club_nom = tr[3].children[8].innerText;// club_nom// 
-var club_region = tr[3].children[10].innerText;// club_region //
-var club_departement = tr[3].children[12].innerText;// club_departement//
+var club_nom = tr[3].children[8].innerText; // club_nom// 
+var club_region = tr[3].children[10].innerText; // club_region //
+var club_departement = tr[3].children[12].innerText; // club_departement//
 
 var totaluser = nomEpreuve.length; // nombre total de user
-console.log(nomEpreuve.length);
-test= 50;
-// totaluser =test;
 
-for(var i=3;i<totaluser-1;i++){ 
- var nombre_space = 0 ;
- var nombre_space_quantite = 0 ; 
- var users_nationality = "" ;
- var users_nom    = "";
- var users_prenom = ""; 
- var premier_lettre =0;
+test = 5;
+totaluser = test;
 
- var totalx= tr[i].children[2].innerText.length; //analyse la perf
-var total_s= tr[i].children[2].innerText; // taille de la perf 
-var res = total_s.replace("''", "."); // remplacement "" par . 
-var reccord="";
-var result_perf="";
-var result_perf2="";
-var result_perf3="";
+for (var i = 3; i < totaluser - 1; i++) {
+	var nombre_space = 0;
+	var nombre_space_quantite = 0;
+	var users_nationality = "";
+	var users_nom = "";
+	var users_prenom = "";
+	var premier_lettre = 0;
+	var totalx = tr[i].children[2].innerText.length; //analyse la perf
+	var total_s = tr[i].children[2].innerText; // taille de la perf 
+	var epreuve_nom = tr[2].innerText;
+	var res = total_s.replace("''", "."); // remplacement "" par . 
+	var reccord = "";
+	var result_perf = "";
+	var result_perf2 = "";
+	var result_perf3 = "";
+	var epreuve_nom_verif = false;
+	var user_sex = "";
+	var reccord_verif = false;
+	var result_perf_verif = false;
+	var result_perf2_verif = false;
+	var result_perf3_verif = false;
+	var users_nationality_verif = false;
+	var users_nom_verif = false;
+	var users_nom_verif = false;
+	var nomcomplet = tr[i].children[6].innerText;
 
-var reccord_verif=false;
-var result_perf_verif=false;
-var result_perf2_verif=false;
-var result_perf3_verif=false;
- 
 
- var users_nationality_verif =false;
- var users_nom_verif   = false;
- var users_nom_verif   = false;
 
- 
 
- 
-// boucle Nom comple
-var nomcomplet= tr[i].children[6].innerText; 
-for(var j = 0; j<nomcomplet.length;j++ ){
-	if(nomcomplet[j]==" "){
-		nombre_space ++ ;
-	}
-	if(nomcomplet[j]=="("){
-		nombre_space ++ ;
-		users_nationality_verif=true; 
-	}
-}
+	// boucle Nom comple
 
-if(users_nationality_verif==false){ // luser est français 
- 
-	users_nationality="FR";
-for(var x = 0 ; x<nomcomplet.length;x++){
- 
-	if(nomcomplet[x]==" "){
-		nombre_space_quantite++; 
-	}
-
-	if(nombre_space_quantite<nombre_space){
-	users_nom  = users_nom +nomcomplet[x] ;
-	
-	}
-	else {		
-		if(nomcomplet[x]!=" "){
-			users_prenom  = users_prenom +nomcomplet[x] ;
-
+	for (var j = 0; j < nomcomplet.length; j++) {
+		if (nomcomplet[j] == " ") {
+			nombre_space++;
 		}
-		
+		if (nomcomplet[j] == "(") {
+			nombre_space++;
+			users_nationality_verif = true;
+		}
 	}
-}
 
+	if (users_nationality_verif == false) { // luser est français 
 
- 
-}
-else { // luser est etranger
-	for(var x = 0 ; x<nomcomplet.length;x++){
-		//console.log(nomcomplet[x]);
-		if(nomcomplet[x]==" "){
-			nombre_space_quantite++; 
-		}
-		if(nombre_space_quantite<nombre_space-2){
-		users_nom  = users_nom +nomcomplet[x] ;
-		}
-		else {
-			
-			if(nomcomplet[x]!="" &&nomcomplet[x]!=" " && nomcomplet[x]!="(" && users_nationality_verif==true ){
-				users_prenom  = users_prenom +nomcomplet[x] ;
+		users_nationality = "FR";
+		for (var x = 0; x < nomcomplet.length; x++) {
+
+			if (nomcomplet[x] == " ") {
+				nombre_space_quantite++;
 			}
-			if(nomcomplet[x]=="("){
-				users_nationality_verif =false;
-			}
-			if(users_nationality_verif==false && nomcomplet[x]!="(" && nomcomplet[x]!=")"){
-				users_nationality = users_nationality+nomcomplet[x];
+
+			if (nombre_space_quantite < nombre_space) {
+				users_nom = users_nom + nomcomplet[x];
+
+			} else {
+				if (nomcomplet[x] != " ") {
+					users_prenom = users_prenom + nomcomplet[x];
+
+				}
+
 			}
 		}
-	}	
-}
 
 
-//console.log(tr[3].children[2].innerText.length+"!?");
 
-// /!\ epreuve 400mheais possible et sprint
-for(var t = 0 ;t<totalx-1;t++){
-	if(res[t]=="R"){
-		reccord = "RP";
+	} else { // luser est etranger
+		for (var x = 0; x < nomcomplet.length; x++) {
+			//console.log(nomcomplet[x]);
+			if (nomcomplet[x] == " ") {
+				nombre_space_quantite++;
+			}
+			if (nombre_space_quantite < nombre_space - 2) {
+				users_nom = users_nom + nomcomplet[x];
+			} else {
+
+				if (nomcomplet[x] != "" && nomcomplet[x] != " " && nomcomplet[x] != "(" && users_nationality_verif == true) {
+					users_prenom = users_prenom + nomcomplet[x];
+				}
+				if (nomcomplet[x] == "(") {
+					users_nationality_verif = false;
+				}
+				if (users_nationality_verif == false && nomcomplet[x] != "(" && nomcomplet[x] != ")") {
+					users_nationality = users_nationality + nomcomplet[x];
+				}
+			}
+		}
 	}
 
 
-	if(res[t]==" "){
-		reccord_verif=true;
+	//console.log(tr[3].children[2].innerText.length+"!?");
+
+	// /!\ epreuve 400mheais possible et sprint
+	for (var t = 0; t < totalx - 1; t++) {
+		if (res[t] == "R") {
+			reccord = "RP";
+		}
+		if (res[t] == " ") {
+			reccord_verif = true;
+		}
+
+		if (reccord_verif == false) {
+			result_perf = result_perf + res[t];
+		} else {
+
+		}
+
+
 	}
 
-	if(reccord_verif==false){
-		result_perf = result_perf+res[t];
+
+	// /!\ epreuve 400mheais possible et sprint
+	// for(var u = 0 ; u< ; u++){
+
+	// }
+
+
+
+
+	console.log("--__--__-__-_-_-_-");
+
+
+
+
+
+
+	for (var f = 0; f < epreuve_nom.length; f++) {
+		if (epreuve_nom[f] == "|") {
+			epreuve_nom_verif = true;
+		}
+		if (epreuve_nom_verif == true) {
+			if (epreuve_nom[f] != "|" && epreuve_nom[f] != " ") {
+				user_sex = user_sex + epreuve_nom[f];
+			}
+		}
 	}
-	else {
+	//console.log(user_sex);
+	//console.log(tr[2].innerText); // epreuve
+	// console.log(result_perf); // perf avec filtre
+	// console.log(reccord); // information si reccord
+	// console.log(users_nationality); // nationalite user
+	// console.log(nomcomplet);
+	// console.log(users_nom); // nom user 
+	// console.log(users_prenom);// prenom user
+	// console.log(tr[i].children[6].innerText);
+	// console.log(tr[i].children[8].innerText);
+	// console.log(tr[i].children[10].innerText);
+	// console.log(tr[i].children[12].innerText);
+	// console.log(tr[i].children[14].innerText);// categorie 
+	// console.log(tr[i].children[16].innerText); // naissance date
+	// console.log(tr[i].children[18].innerText); // jour perf 
+	// console.log(tr[i].children[20].innerText); // jour ville perf 
+	// fin boucle nom comple 
 
-	}
-
-	
-}
-
-console.log(result_perf);
-console.log(reccord);
-// /!\ epreuve 400mheais possible et sprint
-// for(var u = 0 ; u< ; u++){
-	
-// }
- 
- 
-console.log(users_nationality);
-console.log(users_nom);
-console.log(users_prenom);
-// fin boucle nom comple 
-
-
-
-
-
-
-
-
-
-
-
-
- 
-	 //Preparation avant d el'envoie ok 	 
+	//Preparation avant d el'envoie ok 	 
 	// var ok = new Information("php.php"); // création de la classe 
 	// ok.add("club_nom",  tr[i].children[8].innerText); // ajout de l'information pour lenvoi 
 	// ok.add("club_region", tr[i].children[10].innerText); // ajout d'une deuxieme information denvoi  
@@ -213,4 +225,4 @@ console.log(users_prenom);
 	// envoie de donne ok 
 }
 
- //console.log(total); 
+//console.log(total); 
